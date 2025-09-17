3c321d18466c74c098b7d294936f0a50071a59ea
MadThura <thura36094@gmail.com>
2025-09-17 15:09:32 +0630

    global-obj

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('hello world');
+// globalThis.setTimeout(function () {
+//     console.log('helloworld');
+// }, 3000);
+
+// console.log(globalThis.document);
+
+// console.log(__dirname);
+
