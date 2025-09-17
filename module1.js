c6d2609422e46f1c4c53c575df99311a3b02d3af
MadThura <thura36094@gmail.com>
2025-09-17 15:17:13 +0630

    node module system

---
+++ module1.js
@@ -0,0 +1,7 @@
+const myname = 'thura';
+const myage = 22;
+
+module.exports = {
+    myname,
+    myage
+};
---
+++ module2.js
@@ -0,0 +1,3 @@
+const {myname, myage} = require('./module1');
+
+console.log(myname, myage);
