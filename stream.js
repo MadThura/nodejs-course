e3c13bb0795eb12e631035a41344781210c84ce4
MadThura <thura36094@gmail.com>
2025-09-18 16:50:52 +0630

    epi-6-stream-and-buffer

---
+++ stream.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const readStream = fs.createReadStream('./docs/large.txt');
+const writeStream = fs.createWriteStream('./docs/large-write.txt');
+
+// readStream.on('data', function (data) {
+//     writeStream.write(data.toString());
+//     writeStream.write('--chunk--')
+// })
+
+readStream.pipe(writeStream);
